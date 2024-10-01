2748924f7f3783d0056024fc878ad174ee4160e3
PointParticle <168024488+PointParticle@users.noreply.github.com>
2024-10-01 10:50:02 +0200

    Person.py

---
+++ Person.py
@@ -0,0 +1,4 @@
+name = input("Please enter your name: ")
+gender = input("Please enter your gender: ")
+age = input("Please enter your age: ")
+print("You are a", age, "years old", gender, "called", name)
